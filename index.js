e40e6b793e2bb12fe8e9e5feb9e857930f1e52ae
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2019-07-23 15:09:15 -0300

    Include random function

--- index.js
+++ index.js
@@ -79,6 +79,12 @@ parseVariables = (command, message, from, to) => {
 	return command;
 };
 
+random = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+};
+
 app.post('/' + process.env.ROUTE, async (req, res) => {
 	if (!req.body) {
 		res.status(200).send('Ok');
